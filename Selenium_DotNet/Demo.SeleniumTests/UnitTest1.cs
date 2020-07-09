812fa4635c8c9edb1be061dc6cd131fb781f9fdf
virginiayjd7 <37305214+virginiayjd7@users.noreply.github.com>
2020-07-08 20:48:34 -0500

    part1

---
+++ Selenium_DotNet/Demo.SeleniumTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.SeleniumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
